c8fb0f6ede040370b3bef1e54f3dab88f3c43d82
Fabian escobedo <137364972+Fabian3050@users.noreply.github.com>
2026-09-15 23:11:44 -0300

    se agrega metodo para obtener el doi de una publicacion a través del titulo y resumen en los clientes de las fuentes academicas

--- test_ods.py
+++
@@ -1,6 +0,0 @@
-import sys
-import os
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from app.services.ods_service import get_ods_areas_formatted
-
-print(get_ods_areas_formatted())
